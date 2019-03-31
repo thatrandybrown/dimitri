f81e8b3b5a054572f0da02a66a1393ff1c385067
randy brown <randy@beingbrown.net>
2019-03-31 12:44:28 -0500

    provide a base interaction
    this code is mostly extracted from the post by Dimitri DeFigueiredo
    with some small changes made when being leveraged on some internal
    tools. The goal of this extraction is to leverage the similar code
    elsewhere without needing to copy and paste it.

---
+++ index.js
@@ -0,0 +1,47 @@
+var crypto = require('crypto');
+
+if(typeof window !== 'undefined') throw new Error("This module is not safe for client execution");
+
+// crypto above 4 bytes is unreliable
+const getRandomSample = () => crypto.randomBytes(4).readUInt32LE();
+
+// ensure the value is between 0 and 2^32
+const isInExtendedRange = (sample, range) =>
+  sample < (Math.floor(Math.pow(2, 32) / range) * range);
+
+// unsafeCoerce should be removed, preferring safety to success
+const unsafeCoerce = (sample, range) => sample % range;
+
+const sample = (range, rangeCheck, coerce) => {
+  let sample;
+  let i = 0;
+  do {
+    sample = getRandomSample();
+  } while (100 > ++i && !rangeCheck(sample, range));
+  // note that if i is exceeded, the sample may be biased
+  return coerce(sample, range);
+}
+
+// get rand in range could easily invoke sample directly
+const randIntBelow = val => sample(val, isInExtendedRange, unsafeCoerce);
+
+const getRandInRange = (min, max) => {
+  const lo = Math.ceil(min);
+  const hi = Math.floor(max);
+  return lo + randIntBelow(hi - lo + 1);
+};
+
+const getString = validCharsString => {
+  const opts = validCharsString.split("");
+
+  const genStringOfLen = (curr, len) => {
+    if(len <= 0) return curr;
+    return genStringOfLen(curr + opts[getRandInRange(0, opts.length - 1)], len - 1);
+  };
+
+  return len => genStringOfLen("", len);
+};
+
+// console.log(getString("abc")(5));
+
+module.exports = {getRandInRange, getString};
